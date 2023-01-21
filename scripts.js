6d55bac032920cbf35e19630d99bb52c3df52aa8
LucasSilva97 <lucas-silva-santana@hotmail.com>
2023-01-21 15:24:29 -0300

    add a clock

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
+let localClock = document.getElementById("localClock")
 
 button.addEventListener("click", add)
 form.addEventListener("change", save)
@@ -21,6 +22,22 @@ function add() {
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
+
+function showClock() {
+  let clock = new Date()
+
+  clock.hours =
+    clock.getHours() < 10 ? "0" + clock.getHours() : clock.getHours()
+  clock.minutes =
+    clock.getMinutes() < 10 ? "0" + clock.getMinutes() : clock.getMinutes()
+  clock.seconds =
+    clock.getSeconds() < 10 ? "0" + clock.getSeconds() : clock.getSeconds()
+
+  let showClock = `Hora: ${clock.hours}:${clock.minutes}:${clock.seconds}`
+  localClock.innerHTML = showClock
+}
+
+setInterval(showClock, 1000)
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
